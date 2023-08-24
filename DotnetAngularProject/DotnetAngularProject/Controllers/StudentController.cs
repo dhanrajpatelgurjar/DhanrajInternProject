58d98321f5b0f8f00431abb3dbb84a02fe2e3e55
Dharmraj Thakur <thakudh@crowley.com>
2023-08-24 12:43:13 +0530

    a

---
+++ DotnetAngularProject/DotnetAngularProject/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAngularProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public List<string> Student = new List<string>()
+        {
+            "Dhanraj","Suraj","Raj"
+        };
+
+        [HttpGet]
+        public List<string> GetStudent()
+        {
+            return Student;
+        }
+    }
+}
