9f72394e6916d2d986e7eb131691e61befd5b86c
dhanraj <dhanrajpatel17102001@gmail.com>
2023-08-25 19:05:19 +0530

    tet

--- DotnetAngularProject/DotnetAngularProject/Controllers/StudentController.cs
+++ DotnetAngularProject/DotnetAngularProject/Controllers/StudentController.cs
@@ -8,47 +8,38 @@ namespace DotnetAngularProject.Controllers
     [Route("[controller]")]
     public class StudentController : Controller
     {
-        private readonly StudentDetailContext context;
+        private readonly StudentDetailContext _context;
 
         public StudentController(StudentDetailContext context)
         {
-            this.context = context;
+            _context = context;
         }
 
         [HttpGet("GetStudents")]
-        public async Task<ActionResult<List<StudentDetail>>> GetStudents()
+        public async Task<ActionResult<List<StudentDetail>>> Get()
         {
-            var data = await context.StudentDetails.ToListAsync();
+            var data = await _context.StudentDetails.ToListAsync();
             return Ok(data);
         }
 
+/*
         [HttpPost("CreateStudent")]
         public async Task<ActionResult<StudentDetail>> CreateStudent(StudentDetail student)
         {
-            if (ModelState.IsValid)
-            {
-                context.StudentDetails.Add(student);
-                await context.SaveChangesAsync();
-                return CreatedAtAction(nameof(GetStudents), new { id = student.Id }, student);
-            }
-            return BadRequest(ModelState);
+            context.StudentDetails.Add(student);
+            await context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = student.Id }, student);
+        }*/
+
+
+        [HttpPost("CreateStudent")]
+        public async Task<ActionResult<StudentDetail>> Post(StudentDetail student)
+        {
+                _context.StudentDetails.Add(student);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction(nameof(Get), new { id = student.Id }, student);
+            
         }
-
-
-
-
-
-
-        /* [HttpPost("CreateStudent")]
-         public async Task<ActionResult<StudentDetail>> CreateStudent(StudentDetail student)
-         {
-             context.StudentDetails.Add(student);
-             await context.SaveChangesAsync();
-             return student;
-         }*/
-
-
-
 
 
         /*static List<string> student = new List<string>()
